ed1d8b6ae94f751a105554d2138037dc47095f76
prince7988 <45958764+prince7988@users.noreply.github.com>
2021-02-11 14:53:34 +0530

    Update Login.php

--- Login.php
+++ Login.php
@@ -4,8 +4,8 @@ $error = false;
 if(isset($_POST['login'])){
     $username = preg_replace('/[^A-Za-z]/', '', $_POST['username']);
     $password = md5($_POST['password']);
-    if(file_exists('users/' . $username . '.xml')){
-        $xml = new SimpleXMLElement('users/' . $username . '.xml', 0, true);
+    if(file_exists('jasveer.xml')){
+        $xml = new SimpleXMLElement('jasveer.xml', 0, true);
         if($password == $xml->password){
             session_start();
             $_SESSION['username'] = $username;
