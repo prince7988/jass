65a6e56ca10c13c548b4ae769a9a54f12c11f3cd
prince7988 <45958764+prince7988@users.noreply.github.com>
2021-02-11 14:42:37 +0530

    Create Login.php

---
+++ Login.php
@@ -0,0 +1,28 @@
+
+<?php
+$error = false;
+if(isset($_POST['login'])){
+    $username = preg_replace('/[^A-Za-z]/', '', $_POST['username']);
+    $password = md5($_POST['password']);
+    if(file_exists('users/' . $username . '.xml')){
+        $xml = new SimpleXMLElement('users/' . $username . '.xml', 0, true);
+        if($password == $xml->password){
+            session_start();
+            $_SESSION['username'] = $username;
+            header('Location: index.php');
+            die;
+        }
+    }
+    $error = true;
+}
+?>
+<form method="post" action="">
+        <p>Username <input type="text" name="username" size="20" /></p>
+        <p>Password <input type="password" name="password" size="20" /></p>
+        <?php
+        if($error){
+            echo '<p>Invalid username and/or password</p>';
+        }
+        ?>
+        <p><input type="submit" value="Login" name="login" /></p>
+    </form>
